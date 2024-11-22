248d600d8895bca1c5ca4371a0a3070b1c6e841c
zhangguokai <zhangguokai@inspur.com>
2024-11-22 23:49:21 +0800

    feat: 构建主体框架模块

---
+++ build_tsx.sh
@@ -0,0 +1,1 @@
+bun build --watch web/tsx/**/index.tsx --root=./web/tsx --outdir=./static/bmbp_framework/js  --target=browser  --entry-naming=[dir]/[name].[ext]   --external:react --external:react-dom --external:@arco-design/web-react --bundle
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+mod action;
+mod view;
+
+use crate::view::framework_index_view;
+use bmbp_abc::tera_add_template;
+use rust_embed::RustEmbed;
+use salvo::serve_static::static_embed;
+use salvo::Router;
+
+#[derive(RustEmbed)]
+#[folder = "static"]
+struct StaticAssets;
+
+pub fn build_router() -> Router {
+    let mut router = Router::new();
+    router = router.push(Router::with_path("/static/<**path>").get(static_embed::<StaticAssets>()));
+
+    router = router.push(Router::with_path("/framework.view").get(framework_index_view));
+
+    router
+}
+
+#[derive(RustEmbed)]
+#[folder = "web/templates"]
+pub(crate) struct PageAssets;
+pub fn build_template() {
+    for file_path in PageAssets::iter() {
+        tera_add_template(file_path.as_ref(), PageAssets::get(file_path.as_ref()));
+    }
+}
