4f194d98f56ccd38ca3f9826dfd0dcba9ae7aa04
zhangguokai <zhangguokai@inspur.com>
2024-11-23 23:53:52 +0800

    feat: 增加home页跳转

--- src/action/root.rs
+++ src/action/root.rs
@@ -1,11 +1,17 @@
-use bmbp_vars::app_model;
+use bmbp_vars::{app_home_url, app_model};
 use salvo::prelude::*;
-use tera::ast::Node::Text;
 
 #[handler]
 pub async fn root_action(req: &mut Request, resp: &mut Response, depot: &mut Depot) {
     let app_model = app_model();
     if "api".eq_ignore_ascii_case(&app_model) {
-        resp.render("welcome ")
+        resp.render("welcome to bmbp admin ")
+    } else {
+        let home_url = app_home_url();
+        if home_url.is_empty() {
+            resp.render(Redirect::found("/framework.view"))
+        } else {
+            resp.render(Redirect::found(home_url))
+        }
     }
 }
